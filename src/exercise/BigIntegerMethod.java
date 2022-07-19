2bb62e217d06bd4397aeee84c3cd8aae496862f9
Acus <acus1304pgphcm@gmail.com>
2022-07-19 16:21:14 +0700

    Big Integer Method

---
+++ src/exercise/BigIntegerMethod.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigIntegerMethod {
+    public static void main(String[] args) {
+        BigInteger s1, s2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input 2 big integers: ");
+        System.out.println("Number 1: ");
+        s1 = new BigInteger(sc.next());
+        System.out.println("Number 2: ");
+        s2 = new BigInteger(sc.next());
+        System.out.println("The sum is " + s1.add(s2));
+        System.out.println("The subtract is " + s1.subtract(s2));
+        System.out.println("The divide is " + s1.divide(s2));
+        System.out.println("The multiply is " + s1.multiply(s2));
+    }
+}
