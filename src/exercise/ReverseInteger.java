93851827f6eba01ec241669b7d4fd2b3154af4f0
Acus <acus1304pgphcm@gmail.com>
2022-07-20 17:18:26 +0700

    Reverse an integer

---
+++ src/exercise/ReverseInteger.java
@@ -0,0 +1,33 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class ReverseInteger {
+    static void getInput() {
+        while (true) {
+            String s, s1;
+            int s2;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input an integer.");
+            System.out.print("Number: ");
+            s = sc.nextLine();
+            s1 = reverse(s);
+            s2 = Integer.parseInt(s1);
+            System.out.println("The reverse output of the nunber: " + s2);
+        }
+    }
+
+    static String reverse(String s) {
+        String s1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            //The charAt() method returns the character at a specified index (position) in a string.
+            s1 += s.charAt(s.length() - 1 - i);
+        }
+        return s1;
+    }
+
+    public static void main(String[] args) {
+        getInput();
+    }
+}
+
